69c659018016c8bb6764fb99925064e05a40aa93
Praseptya <rezkyu25@gmail.com>
2024-06-11 07:56:41 +0700

    GameFPS

--- mos.py
+++ mos.py
@@ -35,10 +35,9 @@ class Object2_5D(Entity):
         messages = [
             'Cobalah tembak semua target',
             'Gampang kok, tinggal klik aja mouse kiri',
-             'ish jangan ganggu mulu ih',
+            'ish jangan ganggu mulu ih',
             'coba tambahin fitur atau object lainnya',
             'soon tba'
-
         ]
         
         message = messages[self.message_index]
@@ -54,6 +53,52 @@ class Object2_5D(Entity):
 
 object_2_5d = Object2_5D()
 
+class Object2_5D(Entity):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.front = Entity(parent=self, model='quad', texture='char1.png', scale=(3, 3), double_sided=True)
+        self.position = (-5, 1.5, 19)
+        self.message_index = 0
+        self.current_text = None 
+        
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def update(self):
+        direction = camera.world_position - self.world_position
+        angle = atan2(direction.x, direction.z)
+        self.rotation_y = degrees(angle)
+
+    def input(self, key):
+        if key == 'e' and distance(self.world_position, camera.world_position) < 4:
+            self.show_message()
+
+    def show_message(self):
+        if self.current_text:
+            destroy(self.current_text)
+        
+        messages = [
+            'Cobalah tembak semua target',
+            'Gampang kok, tinggal klik aja mouse kiri',
+            'ish jangan ganggu mulu ih',
+            'coba tambahin fitur atau object lainnya',
+            'soon tba'
+        ]
+        
+        message = messages[self.message_index]
+        self.current_text = Text(
+            text=message,
+            origin=(0, 0),
+            position=(0, 0.3),
+            color=color.yellow
+        )
+        self.current_text.fade_out(duration=3)
+        
+        self.message_index = (self.message_index + 1) % len(messages)
+
+
+
+object_2_5d = Object2_5D()
 # 1. field
 
 # Menambahkan banyak kursi dalam baris dan kolom
@@ -63,18 +108,25 @@ chair_texture = load_texture('school chair texture.png')
 for i in range(rows):
     for j in range(cols):
         kursi = Entity(model='school chair.obj', scale=0.4, texture=chair_texture, collider='box', x=-6 + j * 4, z=5 + i * 3)
-        
-ground = Entity(model='plane', scale=(100,1,100), color=color.white.tint(-.2), texture='white_cube', texture_scale=(100,100), collider='box')
+tembok_texture = load_texture('school chair texture.png')  
+
+ground = Entity(model='plane', scale=(100,1,100), color=color.white.tint(-.2), texture='lantai.jpg', texture_scale=(100,100), collider='box')
 barrier = Entity(model='cube', scale=(20,2,0.5), color=color.orange, collider='box', x=0, z=2)
 atap = Entity(model='cube', scale=(24, 1, 24), color=color.white, texture='dinding texture.jpeg', collider='box', x=0, y=9, z=10)
-dinding1 = Entity(model='cube', scale=(20,17,1), color=color.white, texture = 'dinding texture.jpeg',collider='box', x=0, z=-2)
-papan_tulis = Entity (model = 'cube', scale=(8,4,0.5), color = color.white, texture = '200w.gif', collider='mesh', x= 0,y= 3, z= 21)
-papan_tulis2 = Entity (model = 'cube', scale=(10,5,0.5), color = color.white, texture = 'sky.jpg', collider='mesh', x= 0,y= 3, z= -1.5)
-papan_tulis3 = Entity (model = 'cube', scale=(6,3,0.5), color = color.white, texture = 'sky.jpg', collider='mesh', x= 9.7,y= 3, z= 12, rotation_y=90)
-papan_tulis4 = Entity (model = 'cube', scale=(6,3,0.5), color = color.white, texture = 'sky.jpg', collider='mesh', x= 9.7,y= 3, z= 5, rotation_y=90)
-dinding2 = Entity(model='cube', scale=(20,17,1), color=color.white, texture = 'dinding texture.jpeg',collider='box', x=0, z=22)
-dinding3 = Entity(model='cube', scale=(24,17,1), color=color.white, texture = 'dinding texture.jpeg',collider='box', x=-10, z=10, rotation_y=90)
-dinding4 = Entity(model='cube', scale=(24,17,1), color=color.white,texture = 'dinding texture.jpeg', collider='box', x=10, z=10, rotation_y=90)
+dinding1 = Entity(model='cube', scale=(20,17,1), color=color.white, texture='dinding texture.jpeg',collider='box', x=0, z=-2)
+papan_tulis = Entity(model='cube', scale=(8,4,0.5), color=color.white, texture='360_F_219800840_8JfVeADJGyoPx1FWm8x43HYHjiQCy94w.jpg', collider='mesh', x=0, y=3, z=21)
+ac = Entity(model='cube', scale=(4,1,0.1), color=color.white, texture='ac.png', collider='mesh', x=0, y=6, z=21)
+ac = Entity(model='cube', scale=(4,1,0.1), color=color.white, texture='ac.png', collider='mesh', x=9.5,  y=5, z=12, rotation_y=90)
+ac = Entity(model='cube', scale=(4,1,0.1), color=color.white, texture='ac.png', collider='mesh', x=9.5, y=5, z=5, rotation_y=90)
+vents = Entity(model='cube', scale=(4,1,0.1), color=color.white, texture='vents.jpg', collider='mesh', x=-9.5,  y=5, z=12, rotation_y=90)
+vents = Entity(model='cube', scale=(4,1,0.1), color=color.white, texture='vents.jpg', collider='mesh', x=-9.5, y=5, z=5, rotation_y=90)
+pintu = Entity(model='cube', scale=(7,9,0.1), color=color.white, texture='pintu.png', collider='mesh', x=-9.5, y=1, z=19, rotation_y=90)
+papan_tulis2 = Entity(model='cube', scale=(7,4,0.5), color=color.white, texture='sky.jpg', collider='mesh', x=0, y=3, z=-1.5)
+papan_tulis3 = Entity(model='cube', scale=(6,3,0.5), color=color.white, texture='sky.jpg', collider='mesh', x=9.7, y=3, z=12, rotation_y=90)
+papan_tulis4 = Entity(model='cube', scale=(6,3,0.5), color=color.white, texture='sky.jpg', collider='mesh', x=9.7, y=3, z=5, rotation_y=90)
+dinding2 = Entity(model='cube', scale=(20,17,1), color=color.white, texture='dinding texture.jpeg',collider='box', x=0, z=22)
+dinding3 = Entity(model='cube', scale=(24,17,1), color=color.white, texture='dinding texture.jpeg',collider='box', x=-10, z=10, rotation_y=90)
+dinding4 = Entity(model='cube', scale=(24,17,1), color=color.white, texture='dinding texture.jpeg', collider='box', x=10, z=10, rotation_y=90)
 
 lamp_positions = [(-6, 8, 4), (6, 8, 4), (-6, 8, 15), (6, 8, 15)]
 for pos in lamp_positions:
@@ -120,8 +172,6 @@ def input(key):
     if key == 'left mouse down':
         shoot()
         
-        
-
 # 7. Interact object (klo di hit ilang)
 def update():
     global shoot_timer
@@ -159,5 +209,15 @@ def update():
                         message = Text(text='YOU WON', scale=2, origin=(0,0), background=True, color=color.blue)
                         if held_keys['escape']:
                             application.quit()
+
+# Add desk
+wood_texture = load_texture('meja.jpg')
+
+# Desk dimensions
+desk_top = Entity(model='cube', scale=(5, 0.3, 2), texture=wood_texture, collider='box', position=(2, 1, 19))
+desk_leg1 = Entity(model='cube', scale=(0.2, 1, 0.2), texture=wood_texture, collider='box', position=(1.0, 0.5, 18.5))
+desk_leg2 = Entity(model='cube', scale=(0.2, 1, 0.2), texture=wood_texture, collider='box', position=(3, 0.5, 18.5))
+desk_leg3 = Entity(model='cube', scale=(0.2, 1, 0.2), texture=wood_texture, collider='box', position=(1.0, 0.5, 19.5))
+desk_leg4 = Entity(model='cube', scale=(0.2, 1, 0.2), texture=wood_texture, collider='box', position=(3, 0.5, 19.5))
 
 app.run()
